4e5e894abbad02f54030ea614f071e5932bf7065
jaguillo <juloo.dsi@gmail.com>
2015-11-01 10:18:39 +0100

    Update

--- makemake.rb
+++ makemake.rb
@@ -8,10 +8,14 @@ class Makemake < Formula
 	head "https://github.com/Julow/makemake.git"
 	sha256 ""
 
-	version "2.2.0"
+	version "3.0.0"
 
 	def install
-		bin.install "makemake.py" => "makemake"
+		bin.install "old/makemake.py" => "makemake"
+		ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+		system "python", *Language::Python.setup_install_args(libexec)
+		bin.install Dir[libexec/"bin/*"]
+		bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
 	end
 
 	test do
