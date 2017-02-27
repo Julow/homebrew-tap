0f311347ed025a8c7c8a44d324a0b963c7b63728
jaguillo <jaguillo@student.42.fr>
2017-02-27 18:44:49 +0100

    update

--- makemake.rb
+++ makemake.rb
@@ -8,7 +8,7 @@ class Makemake < Formula
 	head "https://github.com/Julow/makemake.git"
 	sha256 ""
 
-	version "3.2.1"
+	version "3.2.2"
 
 	def install
 		bin.install "old/makemake.py" => "makemake"
